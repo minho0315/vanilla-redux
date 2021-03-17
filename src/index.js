806a70c574d428342c76e05fa8fc22a6abcd60ac
minho0315 <dlalsgh369@naver.com>
2021-03-17 23:37:06 +0900

    delete ToDo2

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ const reducer = (state = [], action) => {
     case ADD_TODO:
       return [{text: action.text, id: Date.now() }, ...state];
     case DELETE_TODO:
-      return [];
+      return state.filter(toDo => toDo.id !== action.id);
     default:
       return state;
   }
@@ -42,7 +42,7 @@ const dispatchAddToDo = text => {
 };
 
 const dispatchDeleteToDo = e => {
-  const id = e.target.parentNode.id;
+  const id = parseInt(e.target.parentNode.id);
   store.dispatch(deleteToDo(id));
 };
 
@@ -53,7 +53,7 @@ const paintToDos = () => {
     const li = document.createElement("li");
     const btn = document.createElement("button");
     btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchAddToDo);
+    btn.addEventListener("click", dispatchDeleteToDo);
     li.id = toDo.id;
     li.innerText = toDo.text;
     li.appendChild(btn);
